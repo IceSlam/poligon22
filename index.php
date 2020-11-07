c3e216e4488872b889cef5cbe12f2d237daf39a9
IceSlam <mainguy@mail.ru>
2020-11-07 20:56:00 +0300

    Added Slider on Index page to theme template

--- index.php
+++ index.php
@@ -25,64 +25,32 @@ get_header();
 					<div class="uk-position-relative uk-light" tabindex="-1">
 
 							<ul class="uk-slider-items uk-child-width-1@s uk-grid">
-								<li>
-									<div class="is-slider__slide" style="background: linear-gradient(270deg, rgba(0, 0, 0, 0.3) 0%, rgba(0,0,0,.6) 100%), url(./assets/img/slider_bg1.jpg);">
-										<div class="is-slider__slide-content container-xxl container-xl container">
-											<div class="col-lg-7">
-												<h4 class="sub">
-													Лазертаг с полным погружением
-												</h4>
-												<h3 class="title">
-													в клубе «Полигон 22»
-												</h3>
-												<p class="description">
-													Лазертаг - командная игра и сногсшибательное развлечение для взрослых и детей от 7 лет. Эта игра полностью безопасна и не причиняет болевых ощущений.
-												</p>
-											</div>
-										</div>
-									</div>
-								</li>
-								<li>
-									<div class="is-slider__slide" style="background: linear-gradient(270deg, rgba(0, 0, 0, 0.3) 0%, rgba(0,0,0,.6) 100%), url(./assets/img/slider_bg1.jpg);">
-										<div class="is-slider__slide-content container-xxl container-xl container">
-											<div class="col-lg-7">
-												<h4 class="sub">
-													Лазертаг с полным погружением
-												</h4>
-												<h3 class="title">
-													в клубе «Полигон 22»
-												</h3>
-												<p class="description">
-													Лазертаг - командная игра и сногсшибательное развлечение для взрослых и детей от 7 лет. Эта игра полностью безопасна и не причиняет болевых ощущений.
-												</p>
-											</div>
-										</div>
-									</div>
-								</li>
+								<?php while ( have_rows('index_slides') ) : the_row(); ?>
 									<li>
-										<div class="is-slider__slide" style="background: linear-gradient(270deg, rgba(0, 0, 0, 0.3) 0%, rgba(0,0,0,.6) 100%), url(./assets/img/slider_bg1.jpg);">
+										<div class="is-slider__slide" style="background: linear-gradient(270deg, rgba(0, 0, 0, 0.3) 0%, rgba(0,0,0,.6) 100%), url(<? the_sub_field('background'); ?>);">
 											<div class="is-slider__slide-content container-xxl container-xl container">
 												<div class="col-lg-7">
 													<h4 class="sub">
-														Лазертаг с полным погружением
+														<? the_sub_field('subtitle'); ?>
 													</h4>
 													<h3 class="title">
-														в клубе «Полигон 22»
+														<? the_sub_field('title'); ?>
 													</h3>
 													<p class="description">
-														Лазертаг - командная игра и сногсшибательное развлечение для взрослых и детей от 7 лет. Эта игра полностью безопасна и не причиняет болевых ощущений.
+														<? the_sub_field('description'); ?>
 													</p>
 												</div>
 											</div>
 										</div>
 									</li>
+              <?php endwhile; ?>
 							</ul>
 
 							<a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slider-item="previous">
-								<img src="./assets/img/slider_prev.svg" alt="">
+								<img src="<? echo get_template_directory_uri() . "/assets/img/slider_prev.svg" ?>" alt="">
 							</a>
 							<a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slider-item="next">
-								<img src="./assets/img/slider_prev.svg" alt="" style="transform: rotate(180deg)">
+								<img src="<? echo get_template_directory_uri() . "/assets/img/slider_prev.svg" ?>" alt="" style="transform: rotate(180deg)">
 							</a>
 
 					</div>
