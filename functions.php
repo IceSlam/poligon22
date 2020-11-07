30c2f406966854ee4d68df7cef4e1a3bce10d8e3
IceSlam <mainguy@mail.ru>
2020-11-07 16:19:52 +0300

    Edited functions.php

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 
 if ( ! defined( '_S_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define( '_S_VERSION', '2.0.0' );
 }
 
 if ( ! function_exists( 'poligon22_setup' ) ) :
@@ -141,9 +141,23 @@ add_action( 'widgets_init', 'poligon22_widgets_init' );
  */
 function poligon22_scripts() {
 	wp_enqueue_style( 'poligon22-style', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_enqueue_style( 'Rubik-FiraSans-fonts', 'https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap' );
+	wp_enqueue_style( 'FontAwesome', 'https://use.fontawesome.com/releases/v5.15.1/css/all.css', array(), 5.15);
+	wp_enqueue_style( 'MDBootstrap-style', get_template_directory_uri() . '/assets/css/mdb.min.css', array(), 5.1 );
+	wp_enqueue_style( 'FancyBox-style', get_template_directory_uri() . '/assets/css/jquery.fancybox.min.css', array(), 3.5 );
+	wp_enqueue_style( 'UIKit-style', get_template_directory_uri() . '/assets/css/uikit.min.css', array(), 3.5 );
+	wp_enqueue_style( 'Main-style', get_template_directory_uri() . '/assets/css/main.min.css', array(), _S_VERSION );
+	wp_enqueue_style( 'Media-style', get_template_directory_uri() . '/assets/css/media.min.css', array(), _S_VERSION );
+	wp_enqueue_style( 'AliansLogo-style', get_template_directory_uri() . '/assets/css/alians.min.css', array(), _S_VERSION );
 	wp_style_add_data( 'poligon22-style', 'rtl', 'replace' );
 
 	wp_enqueue_script( 'poligon22-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'jQuery', get_template_directory_uri() . '/assets/js/jquery.3.5.1.min.js', array(), 3.5, true );
+	wp_enqueue_script( 'FancyBox', get_template_directory_uri() . '/assets/js/jquery.fancybox.min.js', array(), 3.5, true );
+	wp_enqueue_script( 'MDBootstrap', get_template_directory_uri() . '/assets/js/mdb.min.js', array(), 5.1, true );
+	wp_enqueue_script( 'UIKit', get_template_directory_uri() . '/assets/js/uikit.min.js', array(), 3.5, true );
+	wp_enqueue_script( 'UIKit-Icons', get_template_directory_uri() . '/assets/js/uikit-icons.min.js', array(), 3.5, true );
+	wp_enqueue_script( 'Poligon22-Scripts', get_template_directory_uri() . '/assets/js/app.min.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -178,3 +192,31 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+/* Custom logo in adminbar */
+
+function poligon22_custom_logo() {
+echo '
+	<style type="text/css">
+	#wpadminbar #wp-admin-bar-wp-logo > .ab-item .ab-icon:before {
+	display:inline-block;
+	background-image: url(' . get_bloginfo('stylesheet_directory') . '/assets/img/poligon_logo.png) !important;
+	background-position: 0 0;
+	width:20px !important;
+	height: 20px !important;
+	color:rgba(0, 0, 0, 0);
+	-webkit-background-size: cover;
+	background-size: cover;
+
+	}
+	#wpadminbar #wp-admin-bar-wp-logo.hover > .ab-item .ab-icon {
+	background-position: center;
+	}
+	</style>
+';
+}
+add_action('wp_before_admin_bar_render', 'poligon22_custom_logo');
+
+function remove_footer_admin () {
+	echo '<p>Тема Полигон 2.0 разработана <a href="https://iceslam.ru" target="_blank">IceSlam</a> в компании <a href="https://alianscompany.ru" target="_blank">Альянс+</a>. Работает на WordPress</p>';
+}
+add_filter('admin_footer_text', 'remove_footer_admin');
