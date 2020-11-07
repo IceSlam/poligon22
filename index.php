56fe4ec81899c6f96a9aeec253dce2b96b6eb7be
IceSlam <mainguy@mail.ru>
2020-11-07 23:41:10 +0300

    Added Weapons block on Index page to theme template

--- index.php
+++ index.php
@@ -190,6 +190,59 @@ get_header();
       </div>
     </section>
 
+		<section class="is-weapons container-xxl container-xl container">
+      <div class="row">
+        <div class="col-lg-9">
+          <h2>
+            Большой арсенал оружия
+          </h2>
+        </div>
+        <div class="col-lg-3">
+          <a href="<? echo get_category_link(5); ?>" class="btn view btn-block text-right">
+            <img src="<? echo get_template_directory_uri() . '/assets/img/weapons_view.svg' ?>" alt="">
+            <span>
+              Смотреть все оружие
+            </span>
+          </a>
+        </div>
+      </div>
+      <div class="row mt-5">
+				<?
+				$args = array(
+					'posts_per_page' => 3,
+					'category__in' => 5
+				);
+
+					$query = new WP_Query( $args );
+					if ( $query->have_posts() ) {
+						while ( $query->have_posts() ) {
+							$query->the_post();
+							?>
+							<div class="col-md-6 col-lg-4 mb-3">
+			          <div class="card">
+			            <div class="img-block">
+			              <img src="<? the_field('weapon_img'); ?>" alt="<? the_title(); ?>" class="img-fluid">
+			            </div>
+			            <a href="<? the_permalink(); ?>" class="title">
+			              <? the_title(); ?>
+			            </a>
+			            <span class="btn plus">
+			              <svg width="11" height="11" viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+			                <line x1="5.5" y1="11" x2="5.5" y2="2.18557e-08" stroke="#84AF5A"/>
+			                <line x1="11" y1="5.5" x2="-4.37113e-08" y2="5.5" stroke="#84AF5A"/>
+			              </svg>
+			            </span>
+			          </div>
+			        </div>
+							<?;
+						}
+					} else {
+					}
+					wp_reset_postdata();
+				?>
+      </div>
+    </section>
+
 
 <?php
 get_footer();
